fa8fbd75a34413c68f91c6d4f5bfcd14b7c41e94
Marcus Perry <marcus.perry2017@gmail.com>
2024-01-06 17:49:37 -0500

    Add files via upload

---
+++ express-jobly/__tests__/companies.test.js
@@ -0,0 +1,17 @@
+const app = require( '../app' );
+const companiesRouter = require( '../routes/companies' );
+
+
+// Test Suite for the Company Routes 
+describe( 'Company Routes', function (){
+
+    // Test 1 
+    test( 'Test for the Compny Routes Homepage', function () {
+        
+    })
+})
+
+
+
+
+
